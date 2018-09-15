1f80e352a23580b87f59aaaf39ec1bd6bfe9896f
neugndestiny <neungdestiny12@gmail.com>
2018-09-15 12:16:12 +0700

    Create first test

--- src/main/java/com/lotto/lotto/controller/AccountController.java
+++ src/main/java/com/lotto/lotto/controller/AccountController.java
@@ -1,11 +1,14 @@
 package com.lotto.lotto.controller;
 
 import com.lotto.lotto.controller.response.AccountResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
-    public AccountResponse getById(int id) {
-        return new AccountResponse();
+    @GetMapping("/account/{id}")
+    public AccountResponse getById(@PathVariable int id) {
+        return new AccountResponse("user", "pass", 1000);
     }
 }
