cdfb59c10e06f07dd859a806d248e995633e77c6
neugndestiny <neungdestiny12@gmail.com>
2018-09-16 14:06:38 +0700

    Rule test of jUnit

--- src/test/java/com/lotto/lotto/controller/AccountControllerErrorUnitTest.java
+++ src/test/java/com/lotto/lotto/controller/AccountControllerErrorUnitTest.java
@@ -5,13 +5,16 @@ import com.lotto.lotto.exception.MyAccountNotFoundException;
 import com.lotto.lotto.model.Account;
 import com.lotto.lotto.repository.AccountRepository;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
 
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class AccountControllerErrorUnitTest {
@@ -27,13 +30,21 @@ public class AccountControllerErrorUnitTest {
         initMocks(this);
     }
 
-    @Test(expected = MyAccountNotFoundException.class)
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
     public void getByIdWithException() {
         // Stub
-        given(repository.findById(2))
-                .willReturn(Optional.empty());
+//        given(repository.findById(2))
+//                .willReturn(Optional.empty());
+        when(repository.findById(2))
+                .thenReturn(Optional.empty());
 
         accountController = new AccountController(repository);
+
+        thrown.expect(MyAccountNotFoundException.class);
+        thrown.expectMessage("Account id=[2] not found");
         AccountResponse response = accountController.getById(2);
     }
 }
