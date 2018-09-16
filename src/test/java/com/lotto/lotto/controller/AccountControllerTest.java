707e10028ff2ba76d1af34667a2130e2c7c8d250
neugndestiny <neungdestiny12@gmail.com>
2018-09-16 15:02:07 +0700

    Add Stub to initial variables before test fixed Id not found bug

--- src/test/java/com/lotto/lotto/controller/AccountControllerTest.java
+++ src/test/java/com/lotto/lotto/controller/AccountControllerTest.java
@@ -2,6 +2,8 @@ package com.lotto.lotto.controller;
 
 import com.lotto.lotto.category.IntegrationTest;
 import com.lotto.lotto.controller.response.AccountResponse;
+import com.lotto.lotto.model.Account;
+import com.lotto.lotto.repository.AccountRepository;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -23,8 +25,18 @@ public class AccountControllerTest {
     @Autowired
     private TestRestTemplate testRestTemplate;
 
+    @Autowired
+    private AccountRepository accountRepository;
+
     @Test
     public void getById() {
+        // Stub
+        Account account = new Account();
+        account.setId(1);
+        account.setUsreName("Neung");
+        account.setPassword("pass");
+        accountRepository.save(account);
+
         ResponseEntity<AccountResponse> result =  testRestTemplate.getForEntity("/account/1", AccountResponse.class);
         assertEquals(HttpStatus.OK, result.getStatusCode());
         AccountResponse expected = new AccountResponse("Neung", "pass", 0);
